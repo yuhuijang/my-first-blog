302df1379c6868c6f6c58806bd0fdbe3d3d7bda9
yuhuijang <yuhuijang@gmail.com>
2015-10-03 16:28:12 +0900

    Changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
-
+from django.conf.urls import include, url
+from django.contrib import admin
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+]
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
